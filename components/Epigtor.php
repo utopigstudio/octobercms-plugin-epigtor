62dd3ec1f1d56c61e64d316a0ef393bd423b3ed1
Maria Vilaró <maria@utopigstudio.com>
2023-11-30 20:35:06 +0100

    bump version

--- components/Epigtor.php
+++ components/Epigtor.php
@@ -116,11 +116,11 @@ class Epigtor extends ComponentBase
 
             $this->paragraphFormats = EditorSetting::getConfiguredFormats('html_paragraph_formats') ? json_encode(EditorSetting::getConfiguredFormats('html_paragraph_formats')) : null;
 
-            $this->addCss('assets/css/epigtor.css?v=2.0.0');
-            $this->addJs('assets/js/epigtor-panel.js?v=2.0.0');
-            $this->addJs('assets/js/epigtor.js?v=2.0.0.1');
-            $this->addJs('assets/js/epigtor-image.js?v=2.0.0');
-            $this->addJs('assets/js/epigtor-link.js?v=2.0.0');
+            $this->addCss('assets/css/epigtor.css?v=3.0.0');
+            $this->addJs('assets/js/epigtor-panel.js?v=3.0.0');
+            $this->addJs('assets/js/epigtor.js?v=3.0.0');
+            $this->addJs('assets/js/epigtor-image.js?v=3.0.0');
+            $this->addJs('assets/js/epigtor-link.js?v=3.0.0');
 
             $this->ace_vendor_path = Url::asset('/plugins/utopigs/epigtor/assets/vendor/oc2/codeeditor/assets/vendor/ace');
 
